747b94ee17370844b81caced9962f311b3635385
cyb3erasad <cyb3rasad@gmail.com>
2026-02-07 17:05:17 +0500

    Add database model and make routes

--- website/models.py
+++ website/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class ChaiVarity(models.Model):
+    CHAI_TYPE_CHOICES = [
+        ('ML', 'Masala'),
+        ('MK', 'Milk'),
+        ('EL', 'Elaichi'),
+        ('GR', 'Ginger'),
+        ('PL', 'Plain')
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chai_image/')
+    date_add = models.DateTimeField(default=timezone.now)
+    chai_type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+    description = models.TextField(default='')
+    price = models.CharField(max_length=20, default="")
+
+    def __str__(self):
+        return self.name
+
--- website/views.py
+++ website/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from . models import ChaiVarity
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
 def all_chai(request):
-    return render(request, 'website/home.html')
+    chais = ChaiVarity.objects.all()
+    return render(request, 'website/home.html', {'chais': chais})
+
+def chai_details(request, chai_id):
+    chai = get_object_or_404(ChaiVarity, pk=chai_id)
+    return render(request, 'website/description.html', {'chai': chai})
