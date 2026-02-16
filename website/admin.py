df3711d3dd2d1986d083f0f946a6f6f6fe3894e2
cyb3erasad <cyb3rasad@gmail.com>
2026-02-17 01:11:18 +0500

    learn relationship and modify admin panel

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import ChaiVarity
+from .models import ChaiVarity, ChaiReview, ChaiStore, ChaiCertificate
 
 # Register your models here.
 
-admin.site.register(ChaiVarity)
+class ChaiReviewInLine(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'chai_type', 'date_add')
+    inlines = [ChaiReviewInLine]
+
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varieties', )
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+
+admin.site.register(ChaiVarity, ChaiVarietyAdmin)
+admin.site.register(ChaiReview)
+admin.site.register(ChaiStore, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
