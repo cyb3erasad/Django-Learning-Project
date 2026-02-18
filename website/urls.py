3dcc4417b7e4a05ab63cb37e19781dbaaddeb760
cyb3erasad <cyb3rasad@gmail.com>
2026-02-19 01:00:44 +0500

    learn and made forms

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_chai, name="all_chai"),
     path('<int:chai_id>/', views.chai_details, name="chai_details"),
+    path('chai_store/', views.chai_store, name="chai_stores"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from . models import ChaiVarity
+from . models import ChaiVarity, ChaiStore
 from django.shortcuts import get_object_or_404
+from . forms import ChaiVarityForm
 # Create your views here.
 
 def all_chai(request):
@@ -10,3 +11,14 @@ def all_chai(request):
 def chai_details(request, chai_id):
     chai = get_object_or_404(ChaiVarity, pk=chai_id)
     return render(request, 'website/description.html', {'chai': chai})
+
+def chai_store(request):
+    if request.method == "POST":
+        form = ChaiVarityForm(request.POST)
+        if form.is_valid():
+            chai_varity = form.cleaned_data['chai_varity']
+            store = ChaiStore.objects.filter(chai_varieties=chai_varity)
+    else:
+        form = ChaiVarityForm()        
+    
+    return render(request, 'website/chai_store.html', {'store': store, 'form': form})
